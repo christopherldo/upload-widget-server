882f1362649a3327b6673bbba629e42fcea234a8
Christopher de Oliveira <christopherforwork@gmail.com>
2025-11-19 16:27:57 -0400

    Add upload image route and configure server error handling

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,33 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
-import { env } from '@/env'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, _, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env.DATABASE_URL)
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
