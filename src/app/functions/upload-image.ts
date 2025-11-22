8d45eeb5f4190be74ac7dbc430831f5cd5e99a71
Christopher de Oliveira <christopherforwork@gmail.com>
2025-11-22 14:48:51 -0400

    feat: implement file upload functionality to Cloudflare R2 and update upload image route

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -2,6 +2,7 @@ import { Readable } from 'node:stream'
 import z from 'zod'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
 import { type Either, makeLeft, makeRight } from '@/shared/either'
 import { InvalidFileFormat } from './errors/invalid-file-format'
 
@@ -18,17 +19,24 @@ const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 export async function uploadImage(
   input: UploadImageInput
 ): Promise<Either<InvalidFileFormat, { url: string }>> {
-  const { contentType, fileName } = uploadImageInput.parse(input)
+  const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
     return makeLeft(new InvalidFileFormat())
   }
 
-  await db.insert(schema.uploads).values({
-    name: fileName,
-    remoteKey: fileName,
-    remoteUrl: fileName,
+  const { key, url } = await uploadFileToStorage({
+    folder: 'images',
+    fileName,
+    contentType,
+    contentStream,
   })
 
-  return makeRight({ url: '' })
+  await db.insert(schema.uploads).values({
+    name: fileName,
+    remoteKey: key,
+    remoteUrl: url,
+  })
+
+  return makeRight({ url })
 }
